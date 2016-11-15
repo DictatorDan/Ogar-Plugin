7895e21c55963f71dd9f8078bbe7d3691e37935d
DictatorDan <daniel1j248@gmail.com>
2016-11-15 15:11:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-module.exports = {
-// [General]
-name: "", // Name of plugin REQUIRED
-author: "", // author REQUIRED
-description: '', // Desciprtion
-minVersion: '', // minimum version requirements (optional)
-version:  '', // version REQUIRED
-addToHelp: false, // add to help
- commands: false, // commands
- configFile: false, //config file
-// [Functions]
-init: function (data,configs) {
-  // init, Used to do stuff such as overriding things
-},
-beforeSpawn: function(data) {
-   return true;
-}
-}
-----------------------------------------------------------------------------------------------------
+
 module.exports = {
 // [General]
 name: "Sample Plugin", // Name of plugin REQUIRED
@@ -26,7 +8,6 @@ minVersion: '1.0.0', // minimum version requirements (optional)
 version:  '1.0.0', // version REQUIRED
 addToHelp: ["You can add to the help command", "like this, useful for adding commands"], // add to help
  commands: {
- test: require('./test.js'), // you can add commands!
  lol: require('./lol.js')
  }, // commands
  configFile: 'config.ini', //config file. You can add configs!
