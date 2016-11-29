3065f697b4df82d398d4c9b3e288993124b43443
DictatorDan <daniel1j248@gmail.com>
2016-11-29 16:13:50 -0500

    Update msg.js

--- msg.js
+++ msg.js
@@ -1,4 +1,17 @@
 module.exports = function(str,main,log) {
-
-
+   //str = typed into console
+  //main = API's
+  //log = console.log
+  
+  str = str.split(" ")
+  var message = str.slice(2).join(" ") 
+  var time = parseInt(str[1])
+  if (isNaN(time)) {
+   return log("cya{[OpenAgar]} Please specify a valid interval time! (In seconds)".styleMe()) 
+  }
+  
+  setInterval(function() {
+  main.broadcast(message,"[" + main.scname + "]",{})
+  
+  },1000 * time)
 }
